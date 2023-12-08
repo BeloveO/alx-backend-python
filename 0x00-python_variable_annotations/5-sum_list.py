c47fbb2aeaef50a07bc45072cb919b661d239e69
Belove <beloveolusola@gmail.com>
2023-12-08 01:40:16 +0100

    Type function sum of float list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from typing import List
+"""
+Type function with list of floats
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    returns sum of the list of floats
+    """
+    return sum(input_list)
