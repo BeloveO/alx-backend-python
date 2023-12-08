8bb4b0f0e57f2fb4a4df969bb66c8838b0747270
Belove <beloveolusola@gmail.com>
2023-12-08 02:16:35 +0100

    Annotate function parameters

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -5,7 +5,7 @@ Annotate the function parameters
 from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     returns appropirate types
     """
