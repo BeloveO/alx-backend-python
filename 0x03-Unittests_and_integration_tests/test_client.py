e3c8a86bf2e89cf4a9904bff3adb959f88aed33c
Belove <beloveolusola@gmail.com>
2024-01-09 02:22:09 +0100

    Unittest expected output

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -105,3 +105,15 @@ class TestGithubOrgClient(unittest.TestCase):
                 )
             mock_public_repos_url.assert_called_once()
         mock_get_json.assert_called_once()
+
+    @parameterized.expand([
+            ({"license": {"key": "my_license"}}, "my_license", True),
+            ({"license": {"key": "other_license"}}, "my_license", False),
+        ])
+    def test_has_license(self, repo: Dict, key: str, expected: bool) -> None:
+        """
+        Test for has_license's expected output
+        """
+        gh_org_client = GithubOrgClient("google")
+        client_has_licence = gh_org_client.has_license(repo, key)
+        self.assertEqual(client_has_licence, expected)
