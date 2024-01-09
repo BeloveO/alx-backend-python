a45f415680e896ae54684db3546de5e7e8e79993
Belove <beloveolusola@gmail.com>
2024-01-09 02:43:24 +0100

    Unittest integration test

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -151,6 +151,24 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         cls.get_patcher = patch("requests.get", side_effect=get_payload)
         cls.get_patcher.start()
 
+    def test_public_repos(self) -> None:
+        """
+        Test for public_repos' expected output
+        """
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(),
+            self.expected_repos
+            )
+
+    def test_public_repos_with_license(self) -> None:
+        """
+        Test public_repos with arg license="apache-2.0" for expected output
+        """
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(license="apache-2.0"),
+            self.apache2_repos
+            )
+
     @classmethod
     def tearDownClass(cls) -> None:
         """
