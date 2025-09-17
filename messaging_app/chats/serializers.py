d002d26513d3bcaa54daa2c805e7821874bc9e9e
Belove <beloveolusola@gmail.com>
2025-09-17 06:50:32 -0600

    Serializers for User, Conversation and Message

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -12,6 +12,11 @@ class UserSerializer(serializers.ModelSerializer):
 class ConversationSerializer(serializers.ModelSerializer):
     # Nested serializer to include participant details
     participants = UserSerializer(many=True, read_only=True)
+    # Nested representation to validate errors in the participants
+    def validate_participants(self, value):
+        if not value:
+            raise serializers.ValidationError("A conversation must have at least one participant.")
+        return value
     class Meta:
         model = Conversation
         fields = ['conversation_id', 'participants_id', 'created_at']
@@ -21,6 +26,12 @@ class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only=True)
     conversation = ConversationSerializer(read_only=True)
     message = serializers.CharField(source='message_body')
+    # Nested representation to validate errors in the message body
+    def validate_message(self, value):
+        if not value:
+            raise serializers.ValidationError("Message body cannot be empty.")
+        return value
+
     class Meta:
         model = Message
         fields = ['message_id', 'conversation_id', 'sender_id', 'message', 'sent_at']
