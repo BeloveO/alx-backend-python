8e1d532d19b964e56cdc11b8119072a5860fd51a
Belove <beloveolusola@gmail.com>
2025-09-17 07:23:18 -0600

    URLs for created Serializers

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -27,14 +27,27 @@ class ConversationViewSet(viewsets.ModelViewSet):
     http_method_names = ['get', 'post', 'put', 'delete']
     lookup_field = 'conversation_id'
     lookup_value_regex = '[0-9]+'
+
+    def get_serializer_class(self):
+        return ConversationSerializer
+    
+    # Get queryset for conversation where current user is a participant
     def get_queryset(self):
-        return Conversation.objects.all()
+        user = self.request.user
+        return Conversation.objects.filter(participants=user)
+    # Get the details of a specific conversation
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+    
     # Implement the endpoints to create a new conversation
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         return Response(serializer.data, status=201)
+    
     # Implement the endpoints to update a conversation
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop('partial', False)
@@ -43,6 +56,7 @@ class ConversationViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
         return Response(serializer.data)
+    
     # Implement the endpoints to delete a conversation
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -55,14 +69,23 @@ class MessageViewSet(viewsets.ModelViewSet):
     http_method_names = ['get', 'post', 'put', 'delete']
     lookup_field = 'message_id'
     lookup_value_regex = '[0-9]+'
+    
+    # Get serializer class for message
+    def get_serializer_class(self):
+        return MessageSerializer
+    
+    # Return only messages in conversations where the user is a participant
     def get_queryset(self):
-        return Message.objects.all()
+        user = self.request.user
+        return Message.objects.filter(conversation__participants=user)
+    
     # Implement the endpoints to create a new message
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         return Response(serializer.data, status=201)
+    
     # Implement the endpoints to update a message
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop('partial', False)
@@ -71,6 +94,7 @@ class MessageViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
         return Response(serializer.data)
+    
     # Implement the endpoints to delete a message
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
