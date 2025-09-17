db8332d193be3604a49893cafdf624e09381b132
Belove <beloveolusola@gmail.com>
2025-09-17 06:34:04 -0600

    Models User, Conversation and Message

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 
 class User(models.Model):
     # extension of the Abstract user for values not defined in the built-in Django User model
-    user_id = models.AutoField(primary_key=True)
+    user_id = models.AutoField(primary_key=True, unique=True)
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30, blank=False)
@@ -16,12 +17,14 @@ class User(models.Model):
 
 
 class Conversation(models.Model):
-    conversation_id = models.AutoField(primary_key=True)
+    # tracks which users are involved in a conversation
+    conversation_id = models.AutoField(primary_key=True, unique=True)
     participants_id = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=models.functions.Now())
 
 class Message(models.Model):
-    message_id = models.AutoField(primary_key=True)
+    # containing the sender, conversation as defined in the shared schema 
+    message_id = models.AutoField(primary_key=True, unique=True)
     conversation_id = models.ForeignKey(Conversation, on_delete=models.CASCADE)
     sender_id = models.ForeignKey(User, on_delete=models.CASCADE)
     message_body = models.TextField()
