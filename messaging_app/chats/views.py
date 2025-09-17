adb96e5f11855e2f3d7daccf9040c6019ed1d2f6
Belove <beloveolusola@gmail.com>
2025-09-17 07:09:07 -0600

    Views for created Serializers

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,78 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view, viewsets
+from rest_framework.response import Response
+from .models import User, Conversation, Message
+from .serializers import UserSerializer, ConversationSerializer, MessageSerializer
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    http_method_names = ['get', 'post', 'put', 'delete']
+    lookup_field = 'user_id'
+    lookup_value_regex = '[0-9]+'
+    def get_queryset(self):
+        return User.objects.all()
+    def perform_create(self, serializer):
+        serializer.save()
+    def perform_update(self, serializer):
+        serializer.save()
+    def perform_destroy(self, instance):
+        instance.delete()
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    http_method_names = ['get', 'post', 'put', 'delete']
+    lookup_field = 'conversation_id'
+    lookup_value_regex = '[0-9]+'
+    def get_queryset(self):
+        return Conversation.objects.all()
+    # Implement the endpoints to create a new conversation
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=201)
+    # Implement the endpoints to update a conversation
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+    # Implement the endpoints to delete a conversation
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=204)
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    http_method_names = ['get', 'post', 'put', 'delete']
+    lookup_field = 'message_id'
+    lookup_value_regex = '[0-9]+'
+    def get_queryset(self):
+        return Message.objects.all()
+    # Implement the endpoints to create a new message
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=201)
+    # Implement the endpoints to update a message
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+    # Implement the endpoints to delete a message
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=204)
